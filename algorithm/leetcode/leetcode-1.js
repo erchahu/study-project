a2d71f7bd2813f1dda10bd131007bd495251d5e5
Sera.Hu <hulixiaSera@gmail.com>
2026-01-28 12:12:17 +0800

    feat: algorithm/leetcode-1.js

---
+++ algorithm/leetcode/leetcode-1.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(nums[i])) { // hasOwnProperty用来判断object中是否存在某个key
+            return [map[nums[i]], i] // map[nums[i] 才是 下标
+        }
+        const returnNum = target - nums[i];
+        map[returnNum] = i;
+    }
+};
+
+
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
+console.log(twoSum([2,7,11,15], 9))
